186c9e85a0e5919a2516d75292eaea2883903644
Nickolas Tushin <kovcheg.studio.2014@gmail.com>
2023-01-24 12:47:50 +0300

    Add Task

---
+++ Task/Program.cs
@@ -0,0 +1,63 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа.
+
+string[] Array = InputArray();
+string[] resultArray = NewArray(Array);
+string first = WriteArray(Array);
+string second = WriteArray(resultArray);
+Console.WriteLine(first + " --> " + second);
+
+string[] InputArray()
+{
+    Console.WriteLine("Введите слова через пробел,по окончании нажмите Enter: ");
+    string? Symbols = Console.ReadLine();
+    if (Symbols == null) {Symbols = "";};
+    char[] separators = new char[] { ',', ' ' };
+    string[] Array = Symbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return Array;
+}
+
+string WriteArray(string[] Array)
+{
+    string stringArray = "[";
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (i == Array.Length - 1)
+        {
+            stringArray += $"\"{Array[i]}\"";
+            break;
+        }
+        stringArray += ($"\"{Array[i]}\", ");
+    }
+    stringArray += "]";
+    return stringArray;
+}
+
+int CountStringSymbols(string[] Array)
+{
+    int count = 0;
+    foreach (string item in Array)
+    {
+        if (item.Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] NewArray(string[] Array)
+{
+    int resultArrayLength = CountStringSymbols(Array);
+    string[] resultArray = new string[resultArrayLength];
+    int i = 0;
+    foreach (string word in Array)
+    {
+        if (word.Length <= 3)
+        {
+            resultArray[i] = word;
+            i++;
+        }
+    }
+    return resultArray;
+}
